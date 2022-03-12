4ea5125329df07368cf5296a9fe566c5ef88e82c
My Name <passivevoice3@gmail.com>
2022-03-12 10:52:20 +0300

    add: typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import Button from './components/UiKit/Button';
+import H1 from './components/UiKit/typography/H1';
+import P from './components/UiKit/typography/P';
 
 export default function App() {
   return (
       <div className='wrapper'>
         <Button theme='danger' color='white' >title</Button>
         <Button theme='danger' size='medium'>genre</Button>
+        <H1>qwssssssssssssssdq</H1>
+        <P theme='red' register='up' font='large' weight='thin'>wssss</P>
       </div>
   );
 }
---
+++ src/components/UiKit/typography/H1.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  text-transform: uppercase;
+  color: white;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 40px;
+  line-height: 49px;
+  letter-spacing: 1px;
+`
+
+export default H1;
